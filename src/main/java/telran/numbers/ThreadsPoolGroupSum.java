bfd5e337341eeb427c6ed3424a19a5b26d0465d8
Stan Mayer <sthumor@gmail.com>
2024-12-07 12:09:47 +0200

    HW46_final

--- src/main/java/telran/numbers/ThreadsPoolGroupSum.java
+++ src/main/java/telran/numbers/ThreadsPoolGroupSum.java
@@ -1,24 +1,20 @@
 package telran.numbers;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
 public class ThreadsPoolGroupSum extends ThreadsGroupSum{
-    private final ExecutorService pool = Executors.newCachedThreadPool();
+
     public ThreadsPoolGroupSum(int[][]groups) {
         super(groups);
     }
     @Override
    protected void startTasks(FutureTask<Long>[] tasks) {
-        try {
-            for (int i = 0; i < tasks.length; i++) {
-                tasks[i] = new FutureTask<>(new OneGroupSum(groups[i]));
-                pool.submit(tasks[i]);
-            }
-        } finally {
-            pool.shutdown();
-        }
+       ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+       Arrays.stream(tasks).forEach(t -> executor.execute(t));
+       executor.shutdown();
     }
 
 
